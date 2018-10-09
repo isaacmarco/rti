56bd20910e52e6480c5ccf1321dfc8f6592a8399
isaacmarco <isaacmarco@gmail.com>
2018-10-10 00:26:23 +0100

    COMMIT

--- rti/urls.py
+++ rti/urls.py
@@ -8,9 +8,9 @@ from rtiapp.views import \
     nuevo_grupo, editar_grupo,  lista_grupos_evaluador,\
     nuevo_alumno,editar_alumno,\
     listar_alumnos_evaluador_en_grupo,\
-    listar_evaluaciones,nueva_evaluacion, editar_evaluacion, alta_evaluador,documentos,informe_grupo,\
+    listar_evaluaciones,nueva_evaluacion, editar_evaluacion, alta_evaluador,informe_grupo,\
     establecer_curso,actualizar_curso,compartir_grupo,actualizar_grupo_compartido,exportar,exportar_CSV, \
-    cerrar_sesion,informe_individual, upload_csv, eliminar_alumno
+    cerrar_sesion,informe_individual, importar_csv, eliminar_alumno,listar_alumnos_evaluador, documentos
 
 
 urlpatterns = [
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^documentos/', documentos),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^cerrar-sesion/', cerrar_sesion),
-    url(r'^importar-evaluadores/', upload_csv),
+    url(r'^importar-evaluadores/', importar_csv),
 
     # exportar datos
     url(r'^exportar/', exportar),
@@ -49,6 +49,8 @@ urlpatterns = [
     url(r'^editar-alumno/', editar_alumno),
     url(r'^eliminar-alumno/', eliminar_alumno),
     url(r'^lista-alumnos-grupo/', listar_alumnos_evaluador_en_grupo),
+    url(r'^lista-alumnos/', listar_alumnos_evaluador),
+
 
     # evaluaciones
     url(r'^lista-evaluaciones/', listar_evaluaciones),
--- rtiapp/forms.py
+++ rtiapp/forms.py
@@ -10,7 +10,6 @@ from .models import Evaluador, Grupo, Alumno, \
 
 
 class SignUpForm(UserCreationForm):
-
     # campos complementarios
     nombre = forms.CharField(max_length=50, required=True, help_text='Su nombre completo')
     email = forms.EmailField(max_length=254, required=True, help_text='Direccion de correo')
@@ -20,8 +19,6 @@ class SignUpForm(UserCreationForm):
     nivel_academico = forms.ChoiceField(choices=Globales.NIVEL_ACADEMICO_OPCIONES, required=True)
     profesion = forms.ChoiceField(choices=Globales.PROFESION_OPCIONES, required=True)
     zona = forms.ChoiceField(choices=Globales.ZONA_OPCIONES, required=True)
-
-
     class Meta:
         model = User
         fields = ('username', 'nombre', 'email', 'password1', 'password2',
@@ -29,13 +26,8 @@ class SignUpForm(UserCreationForm):
 
 
 
-
-
-
-
-
 # forms IPAE
-
+# todo faltan los valores maximos y minimos
 class Form_Evaluacion_IPAE_PRIMERO(forms.ModelForm):
     class Meta:
         model = Evaluacion_IPAE_PRIMERO
@@ -59,34 +51,48 @@ class Form_Evaluacion_IPAE_TERCERO(forms.ModelForm):
 
 
 class Form_Evaluacion_IPAM_TERCERO(forms.ModelForm):
-    class Meta:
-        model = Evaluacion_IPAM_TERCERO
-        fields = '__all__'
-        exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador','mes_leible')
-
-class Form_Evaluacion_IPAM_SEGUNDO(forms.ModelForm):
-    # TODO OJO CON ESTO ESTA TODO PROVISIONAL HAST Q SE CONFIRME
-
     CN = forms.IntegerField(min_value=0, max_value=64, label='CN')
     ODD = forms.IntegerField(min_value=0, max_value=45, label='ODD')
     SN = forms.IntegerField(min_value=0, max_value=45, label='SN')
     OUD = forms.IntegerField(min_value=0, max_value=45, label='OUD')
     VP = forms.IntegerField(min_value=0, max_value=45, label='VP')
+    class Meta:
+        model = Evaluacion_IPAM_TERCERO
+        fields = '__all__'
+        exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador','mes_leible')
 
 
-
+class Form_Evaluacion_IPAM_SEGUNDO(forms.ModelForm):
+    CN = forms.IntegerField(min_value=0, max_value=64, label='CN')
+    ODD = forms.IntegerField(min_value=0, max_value=45, label='ODD')
+    SN = forms.IntegerField(min_value=0, max_value=45, label='SN')
+    OUD = forms.IntegerField(min_value=0, max_value=45, label='OUD')
+    VP = forms.IntegerField(min_value=0, max_value=45, label='VP')
     class Meta:
         model = Evaluacion_IPAM_SEGUNDO
         fields = '__all__'
         exclude = ('prueba','curso_academico','mes','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
+
 class Form_Evaluacion_IPAM_PRIMERO(forms.ModelForm):
+    CN = forms.IntegerField(min_value=0, max_value=64, label='CN')
+    ODD = forms.IntegerField(min_value=0, max_value=45, label='ODD')
+    SN = forms.IntegerField(min_value=0, max_value=45, label='SN')
+    OUD = forms.IntegerField(min_value=0, max_value=45, label='OUD')
+    VP = forms.IntegerField(min_value=0, max_value=45, label='VP')
     class Meta:
         model = Evaluacion_IPAM_PRIMERO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
+
 class Form_Evaluacion_IPAM_INFANTIL(forms.ModelForm):
+    CN = forms.IntegerField(min_value=0, max_value=64, label='CN')
+    EC = forms.IntegerField(min_value=0, max_value=36, label='EC')
+    SN = forms.IntegerField(min_value=0, max_value=36, label='SN')
+    IN = forms.IntegerField(min_value=0, max_value=63, label='IN')
+    CVA = forms.IntegerField(min_value=0, max_value=100, label='CVA')
+    CM = forms.IntegerField(min_value=0, max_value=40, label='CM')
     class Meta:
         model = Evaluacion_IPAM_INFANTIL
         fields = '__all__'
@@ -96,6 +102,13 @@ class Form_Evaluacion_IPAM_INFANTIL(forms.ModelForm):
 # forms IPAL
 
 class Form_Evaluacion_IPAL_INFANTIL(forms.ModelForm):
+    ADIVINANZAS = forms.IntegerField(min_value=0, max_value=20, label='ADIVINANZAS')
+    CSL = forms.IntegerField(min_value=0, max_value=100, label='CSL')
+    CNL = forms.IntegerField(min_value=0, max_value=100, label='CNL')
+    CLE_IMAGEN = forms.IntegerField(min_value=0, max_value=35, label='CLE_IMAGEN')
+    CLE_TEXTO = forms.IntegerField(min_value=0, max_value=6, label='CLE_TEXTO')
+    CFA = forms.IntegerField(min_value=0, max_value=80, label='CFA')
+
     class Meta:
         model = Evaluacion_IPAL_INFANTIL
         fields = '__all__'
@@ -104,6 +117,14 @@ class Form_Evaluacion_IPAL_INFANTIL(forms.ModelForm):
 
 
 class Form_Evaluacion_IPAL_PRIMERO(forms.ModelForm):
+    TM = forms.IntegerField(min_value=0, max_value=20, label='TM')
+    LP = forms.IntegerField(min_value=0, max_value=40, label='LP')
+    CSL = forms.IntegerField(min_value=0, max_value=100, label='CSL')
+    CNL = forms.IntegerField(min_value=0, max_value=100, label='CNL')
+    FLO = forms.IntegerField(min_value=0, max_value=133, label='FLO')
+    CLE_TEXTO = forms.IntegerField(min_value=0, max_value=6, label='CLE_TEXTO')
+    CFS = forms.IntegerField(min_value=0, max_value=85, label='CFS')
+
     class Meta:
         model = Evaluacion_IPAL_PRIMERO
         fields = '__all__'
@@ -111,14 +132,26 @@ class Form_Evaluacion_IPAL_PRIMERO(forms.ModelForm):
 
 
 class Form_Evaluacion_IPAL_SEGUNDO(forms.ModelForm):
+    CNL = forms.IntegerField(min_value=0, max_value=100, label='CNL')
+    LP = forms.IntegerField(min_value=0, max_value=40, label='LP')
+    TM = forms.IntegerField(min_value=0, max_value=20, label='TM')
+    PRO = forms.IntegerField(min_value=0, max_value=133, label='PRO')
+    # tareas complementarias
+    CSL_ACIERTOS =  forms.IntegerField(min_value=0, max_value=100, label='CSL ACIERTOS')
+    CSL_TIEMPO =  forms.IntegerField(min_value=0, max_value=100, label='CSL TIEMPO')
+    CLE_TEXTO_ACIERTOS =  forms.IntegerField(min_value=0, max_value=100, label='CLE TEXTO ACIERTOS')
+    CLE_TEXTO_TIEMPO =  forms.IntegerField(min_value=0, max_value=100, label='CLE TEXTO TIEMPO')
+    CFS_ACIERTOS =  forms.IntegerField(min_value=0, max_value=100, label='CFS ACIERTOS')
+    CFS_TIEMPO =  forms.IntegerField(min_value=0, max_value=100, label='CFS TIEMPO')
+    VOC_ACIERTOS =  forms.IntegerField(min_value=0, max_value=100, label='VOC ACIERTOS')
+    VOC_TIEMPO =  forms.IntegerField(min_value=0, max_value=100, label='VOC TIEMPO')
+
     class Meta:
         model = Evaluacion_IPAL_SEGUNDO
         fields = '__all__'
         # la prueba IPAL segundo tiene una serie de subpruebas complementarias
         # con puntuaciones directas que el programa debe calcular:
         # CSL, CLE_TEXTO, CFS, VOC
-
-
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus',
                    'evaluador', 'observaciones', 'mes_leible',
                    'CSL', 'CLE_TEXTO', 'CFS', 'VOC',)
@@ -178,4 +211,4 @@ class FormEvaluador(forms.ModelForm):
     class Meta:
         model = Evaluador
         fields = '__all__'
-        exclude = ('usuario', 'informacion_adicional_completa')
+        exclude = ('usuario', 'informacion_adicional_completa', 'centro_pilotaje')
