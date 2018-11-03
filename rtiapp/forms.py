3724642e715d6f5f63adfbec64708da495e90d3d
isaacmarco <isaacmarco@gmail.com>
2018-11-03 12:48:35 +0000

    form labels

--- rtiapp/forms.py
+++ rtiapp/forms.py
@@ -29,110 +29,150 @@ class SignUpForm(UserCreationForm):
 # forms IPAE
 
 class Form_Evaluacion_IPAE_PRIMERO(forms.ModelForm):
-    TLC_1 = forms.IntegerField(required=False,min_value=0, max_value=108, label='TLC')
-    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False,min_value=0, max_value=20, label='DOA')
-    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False,min_value=0, max_value=20, label='DOR')
-    DICTADO_PSEUDOPALABRAS = forms.IntegerField(required=False,min_value=0, max_value=20, label='DP')
-    DICTADO_FRASES = forms.IntegerField(required=False,min_value=0, max_value=21, label='DF')
+    tarea1 = 'Pasar de mayúscula a minúscula'
+    tarea2 = 'Dictado de palabras con ortografía arbitraria'
+    tarea3 = 'Dictado de palabras con ortografía reglada'
+    tarea4 = 'Dictado de pseudopalabras'
+    tarea5 = 'Dictado de frases'
+    TLC_1 = forms.IntegerField(required=False,min_value=0, max_value=108, label=tarea1)
+    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea2)
+    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea3)
+    DICTADO_PSEUDOPALABRAS = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea4)
+    DICTADO_FRASES = forms.IntegerField(required=False,min_value=0, max_value=21, label=tarea5)
     class Meta:
         model = Evaluacion_IPAE_PRIMERO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 class Form_Evaluacion_IPAE_SEGUNDO(forms.ModelForm):
-    TLC_1 = forms.IntegerField(required=False, min_value=0, max_value=108, label='TLC')
-    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False, min_value=0, max_value=20, label='DOA')
-    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False, min_value=0, max_value=20, label='DOR')
-    DICTADO_PSEUDOPALABRAS = forms.IntegerField(required=False, min_value=0, max_value=20, label='DP')
-    DICTADO_FRASES = forms.IntegerField(required=False, min_value=0, max_value=21, label='DF')
+    tarea1 = 'Pasar de mayúscula a minúscula'
+    tarea2 = 'Dictado de palabras con ortografía arbitraria'
+    tarea3 = 'Dictado de palabras con ortografía reglada'
+    tarea4 = 'Dictado de pseudopalabras'
+    tarea5 = 'Dictado de frases'
+    TLC_1 = forms.IntegerField(required=False, min_value=0, max_value=108, label=tarea1)
+    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False, min_value=0, max_value=20, label=tarea2)
+    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False, min_value=0, max_value=20, label=tarea3)
+    DICTADO_PSEUDOPALABRAS = forms.IntegerField(required=False, min_value=0, max_value=20, label=tarea4)
+    DICTADO_FRASES = forms.IntegerField(required=False, min_value=0, max_value=21, label=tarea5)
     class Meta:
         model = Evaluacion_IPAE_SEGUNDO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
+
+
 class Form_Evaluacion_IPAE_TERCERO(forms.ModelForm):
-    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False, min_value=0, max_value=20, label='DOA')
-    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False, min_value=0, max_value=20, label='DOR')
-    DICTADO_FRASES = forms.IntegerField(required=False, min_value=0, max_value=21, label='DF')
-    SEC_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label='SEC5')
-    SEC_SEI_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label='SEC_SEI5')
-    PDC_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label='PDC5')
+    tarea1 = 'Dictado de palabras con ortografía arbitraria'
+    tarea2 = 'Dictado de palabras con ortografía reglada'
+    tarea3 = 'Dictado de frases'
+    tarea4 = 'Escritura de una historia (SEC-5)'
+    tarea5 = 'Escritura de una historia (SEC-SEI-5)'
+    tarea6 = 'Escritura de una historia (PDC-5)'
+    DICTADO_ORTOGRAFIA_ARBITRARIA = forms.IntegerField(required=False, min_value=0, max_value=20, label=tarea1)
+    DICTADO_ORTOGRAFIA_REGLADA = forms.IntegerField(required=False, min_value=0, max_value=20, label=tarea2)
+    DICTADO_FRASES = forms.IntegerField(required=False, min_value=0, max_value=21, label=tarea3)
+    SEC_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label=tarea4)
+    SEC_SEI_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label=tarea5)
+    PDC_5 = forms.IntegerField(required=False, min_value=0, max_value=80, label=tarea6)
     class Meta:
         model = Evaluacion_IPAE_TERCERO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 
+
 # forms IPAM
 
 
 class Form_Evaluacion_IPAM_TERCERO(forms.ModelForm):
-    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label='CN')
-    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label='ODD')
-    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label='SN')
-    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label='OUD')
-    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label='VP')
+    tarea1 = 'Comparación Numérica (CN)'
+    tarea2 = 'Operaciones de dos dígitos (ODD)'
+    tarea3 = 'Secuencias Numéricas (SN)'
+    tarea4 = 'Operaciones de un dígito (OUD)'
+    tarea5 = 'Valor de Posición (VP)'
+    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label=tarea1)
+    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea2)
+    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea3)
+    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea4)
+    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea5)
     class Meta:
         model = Evaluacion_IPAM_TERCERO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador','mes_leible')
 
 
+
 class Form_Evaluacion_IPAM_SEGUNDO(forms.ModelForm):
-    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label='CN')
-    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label='ODD')
-    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label='SN')
-    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label='OUD')
-    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label='VP')
+    tarea1 = 'Comparación Numérica (CN)'
+    tarea2 = 'Operaciones de dos dígitos (ODD)'
+    tarea3 = 'Secuencias Numéricas (SN)'
+    tarea4 = 'Operaciones de un dígito (OUD)'
+    tarea5 = 'Valor de Posición (VP)'
+    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label=tarea1)
+    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea2)
+    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea3)
+    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea4)
+    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea5)
     class Meta:
         model = Evaluacion_IPAM_SEGUNDO
         fields = '__all__'
         exclude = ('prueba','curso_academico','mes','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 
-class Form_Evaluacion_IPAM_PRIMERO(forms.ModelForm):
-    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label='CN')
-    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label='ODD')
-    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label='SN')
-    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label='OUD')
-    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label='VP')
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        self.fields['ODD'] = 5
-        #self.fields['ODD'].widget.attrs['disabled'] = True
-    '''
+class Form_Evaluacion_IPAM_PRIMERO(forms.ModelForm):
+    tarea1 = 'Comparación Numérica (CN)'
+    tarea2 = 'Operaciones de dos dígitos (ODD)'
+    tarea3 = 'Secuencias Numéricas (SN)'
+    tarea4 = 'Operaciones de un dígito (OUD)'
+    tarea5 = 'Valor de Posición (VP)'
+    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label=tarea1)
+    ODD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea2)
+    SN = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea3)
+    OUD = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea4)
+    VP = forms.IntegerField(required=False,min_value=0, max_value=45, label=tarea5)
     class Meta:
         model = Evaluacion_IPAM_PRIMERO
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 
-
-
 class Form_Evaluacion_IPAM_INFANTIL(forms.ModelForm):
-    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label='CN')
-    EC = forms.IntegerField(required=False,min_value=0, max_value=36, label='EC')
-    SN = forms.IntegerField(required=False,min_value=0, max_value=36, label='SN')
-    IN = forms.IntegerField(required=False,min_value=0, max_value=63, label='IN')
-    CVA = forms.IntegerField(required=False,min_value=0, max_value=100, label='CVA')
-    CM = forms.IntegerField(required=False,min_value=0, max_value=40, label='CM')
+    tarea1 = 'Comparación Numérica (CN)'
+    tarea2 = 'Estimación Cantidad (EC)'
+    tarea3 = 'Secuencias Numéricas (SN)'
+    tarea4 = 'Identificación Numérica (IN)'
+    tarea5 = 'Conteo Voz Alta (CVA)'
+    tarea6 = 'Comparación Magnitud (CM) '
+    CN = forms.IntegerField(required=False,min_value=0, max_value=64, label=tarea1)
+    EC = forms.IntegerField(required=False,min_value=0, max_value=36, label=tarea2)
+    SN = forms.IntegerField(required=False,min_value=0, max_value=36, label=tarea3)
+    IN = forms.IntegerField(required=False,min_value=0, max_value=63, label=tarea4)
+    CVA = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea5)
+    CM = forms.IntegerField(required=False,min_value=0, max_value=40, label=tarea6)
     class Meta:
         model = Evaluacion_IPAM_INFANTIL
         fields = '__all__'
         exclude = ('curso_academico','mes','prueba','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 
+
 # forms IPAL
 
 class Form_Evaluacion_IPAL_INFANTIL(forms.ModelForm):
-    ADIVINANZAS = forms.IntegerField(required=False,min_value=0, max_value=20, label='ADIVINANZAS')
-    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CSL')
-    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CNL')
-    CLE_IMAGEN = forms.IntegerField(required=False,min_value=0, max_value=35, label='CLE_IMAGEN')
-    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=6, label='CLE_TEXTO')
-    CFA = forms.IntegerField(required=False,min_value=0, max_value=80, label='CFA')
+    tarea1 = 'Adivinanzas (ADI)'
+    tarea2 = 'Conocimiento Sonido Letra (CSL)'
+    tarea3 = 'Conocimiento Nombre Letra (CNL)'
+    tarea4 = 'CLE-imagen'
+    tarea5 = 'CLE-texto'
+    tarea6 = 'Conciencia Fonológica Aislar (CFA)'
+    ADIVINANZAS = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea1)
+    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea2)
+    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea3)
+    CLE_IMAGEN = forms.IntegerField(required=False,min_value=0, max_value=35, label=tarea4)
+    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=6, label=tarea5)
+    CFA = forms.IntegerField(required=False,min_value=0, max_value=80, label=tarea6)
     class Meta:
         model = Evaluacion_IPAL_INFANTIL
         fields = '__all__'
@@ -141,30 +181,47 @@ class Form_Evaluacion_IPAL_INFANTIL(forms.ModelForm):
 
 
 class Form_Evaluacion_IPAL_PRIMERO(forms.ModelForm):
-    TM = forms.IntegerField(required=False,min_value=0, max_value=20, label='TM')
-    LP = forms.IntegerField(required=False,min_value=0, max_value=40, label='LP')
-    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CSL')
-    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CNL')
-    FLO = forms.IntegerField(required=False,min_value=0, max_value=133, label='FLO')
-    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=6, label='CLE_TEXTO')
-    CFS = forms.IntegerField(required=False,min_value=0, max_value=85, label='CFS')
+    tarea1 = 'Textos Mutilados (TM)'
+    tarea2 = 'Lectura de Pseudopalabras (LP)'
+    tarea3 = 'Conocimiento Sonido Letra (CSL)'
+    tarea4 = 'Conocimiento Nombre Letra (CNL)'
+    tarea5 = 'Fluidez Lectura Oral (FLO)'
+    tarea6 = 'CLE-texto'
+    tarea7 = 'Conciencia Fonológica Segmentar (CFS)'
+    TM = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea1)
+    LP = forms.IntegerField(required=False,min_value=0, max_value=40, label=tarea2)
+    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea3)
+    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea4)
+    FLO = forms.IntegerField(required=False,min_value=0, max_value=133, label=tarea5)
+    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=6, label=tarea6)
+    CFS = forms.IntegerField(required=False,min_value=0, max_value=85, label=tarea7)
     class Meta:
         model = Evaluacion_IPAL_PRIMERO
         fields = '__all__'
         exclude = ('curso_academico','prueba','mes','alumno', 'tipo', 'momento', 'riesgo', 'omnibus', 'evaluador', 'mes_leible')
 
 
+
 class Form_Evaluacion_IPAL_SEGUNDO(forms.ModelForm):
-    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CNL')
-    LP = forms.IntegerField(required=False,min_value=0, max_value=40, label='LP')
-    TM = forms.IntegerField(required=False,min_value=0, max_value=20, label='TM')
-    PRO = forms.IntegerField(required=False,min_value=0, max_value=133, label='PRO')
-    FLO = forms.IntegerField(required=False,min_value=0, max_value=133, label='FLO')
+    tarea1 = 'Conocimiento Nombre Letra (CNL)'
+    tarea2 = 'Lectura de Pseudopalabras (LP)'
+    tarea3 = 'Textos Mutilados (TM)'
+    tarea4 = 'Prosodia (PRO)'
+    tarea5 = 'Fluidez Lectura Oral (FLO)'
+    tarea6 = 'Conocimiento Sonido Letra (CSL)'
+    tarea7 = 'CLEtexto'
+    tarea8 = 'Conciencia Fonológica Segmentar (CFS)'
+    tarea9 = 'Vocabulario (VOC)'
+    CNL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea1)
+    LP = forms.IntegerField(required=False,min_value=0, max_value=40, label=tarea2)
+    TM = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea3)
+    PRO = forms.IntegerField(required=False,min_value=0, max_value=133, label=tarea4)
+    FLO = forms.IntegerField(required=False,min_value=0, max_value=133, label=tarea5)
     # tareas complementarias 'CSL', 'CLE_TEXTO', 'CFS', 'VOC',
-    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label='CSL')
-    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=20, label='CLE TEXTO')
-    CFS = forms.IntegerField(required=False,min_value=0, max_value=85, label='CFS')
-    VOC = forms.IntegerField(required=False,min_value=0, max_value=30, label='VOC')
+    CSL = forms.IntegerField(required=False,min_value=0, max_value=100, label=tarea6)
+    CLE_TEXTO = forms.IntegerField(required=False,min_value=0, max_value=20, label=tarea7)
+    CFS = forms.IntegerField(required=False,min_value=0, max_value=85, label=tarea8)
+    VOC = forms.IntegerField(required=False,min_value=0, max_value=30, label=tarea9)
     class Meta:
         model = Evaluacion_IPAL_SEGUNDO
         fields = '__all__'
