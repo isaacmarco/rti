802c5e83eb2bbf02fd616f132c3415d457bce515
isaacmarco <isaacmarco@gmail.com>
2018-11-20 22:32:10 +0000

    commit

--- rtiapp/riesgo_ipam.py
+++ rtiapp/riesgo_ipam.py
@@ -7,17 +7,15 @@ import rtiapp.constantes as Globales
 
 def riesgo_INFANTIL(omnibus, momento):
 
-
-
     if momento == Globales.INICIO:
 
-        if (omnibus <= -0.3351722):
+        if (omnibus <= -0.317927540):
             return Globales.RIESGO
-        if (omnibus > -0.3351722  and omnibus <= -0.1328701):
+        if (omnibus > -0.317927540 and omnibus <= -0.1566956849):
             return Globales.BAJO
-        if (omnibus > -0.1328701 and omnibus <= 0.4237160):
+        if (omnibus > -0.1566956849 and omnibus <= 0.191346370):
             return Globales.NORMAL
-        if (omnibus > 0.4237160):
+        if (omnibus > 0.191346370):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL INFANTIL')
         return 404
@@ -28,22 +26,22 @@ def riesgo_INFANTIL(omnibus, momento):
             return Globales.RIESGO
         if (omnibus > -0.2996457 and omnibus <= -0.1311223):
             return Globales.BAJO
-        if (omnibus > -0.1311223 and omnibus <= 0.3529345):
+        if (omnibus > -0.1311223 and omnibus <= 0.097948452):
             return Globales.NORMAL
-        if (omnibus > 0.3529345):
+        if (omnibus > 0.097948452):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL INFANTIL')
         return 404
 
     if momento == Globales.FIN:
 
-        if (omnibus <= -0.2292327):
+        if (omnibus <= -0.2677698249):
             return Globales.RIESGO
-        if (omnibus > -0.2292327 and omnibus <= -0.07182318):
+        if (omnibus > -0.2677698249 and omnibus <= -0.07182318):
             return Globales.BAJO
-        if (omnibus > -0.07182318 and omnibus <= 0.2602976):
+        if (omnibus > -0.07182318 and omnibus <= 0.1023734856):
             return Globales.NORMAL
-        if (omnibus > 0.2602976):
+        if (omnibus > 0.1023734856):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL INFANTIL')
         return 404
@@ -58,25 +56,25 @@ def riesgo_PRIMERO(omnibus, momento):
             return Globales.ALTO_RIESGO
         if (omnibus > -0.347999306  and omnibus <= -0.07087435):
             return Globales.RIESGO
-        if (omnibus > -0.07087435  and omnibus <= -0.019275252):
+        if (omnibus > -0.07087435  and omnibus <= 0.097947505):
             return Globales.BAJO
-        if (omnibus > -0.019275252 and omnibus <= 0.013140487):
+        if (omnibus > 0.097947505 and omnibus <= 0.334823181):
             return Globales.NORMAL
-        if (omnibus > 0.013140487):
+        if (omnibus > 0.334823181):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL PRIMERO')
         return 404
 
     if momento == Globales.MEDIO:
-        if (omnibus <= -0.342912398):
+        if (omnibus <= -0.333712733):
             return Globales.ALTO_RIESGO
-        if (omnibus > -0.342912398 and omnibus <= -0.112608529):
+        if (omnibus > -0.333712733 and omnibus <= -0.112608529):
             return Globales.RIESGO
         if (omnibus > -0.112608529 and omnibus <= 0.100530127):
             return Globales.BAJO
-        if (omnibus > 0.100530127 and omnibus <= 0.162980798):
+        if (omnibus > 0.100530127 and omnibus <= 0.3091813822):
             return Globales.NORMAL
-        if (omnibus > 0.162980798):
+        if (omnibus > 0.3091813822):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL PRIMERO')
         return 404
@@ -86,11 +84,11 @@ def riesgo_PRIMERO(omnibus, momento):
             return Globales.ALTO_RIESGO
         if (omnibus > -0.198908681 and omnibus <= 0.018302796):
             return Globales.RIESGO
-        if (omnibus > 0.018302796 and omnibus <= 0.114669608):
+        if (omnibus > 0.018302796 and omnibus <= 0.2311316100):
             return Globales.BAJO
-        if (omnibus > 0.114669608 and omnibus <= 0.137982938):
+        if (omnibus > 0.2311316100 and omnibus <= 0.3431618169):
             return Globales.NORMAL
-        if (omnibus > 0.137982938):
+        if (omnibus > 0.3431618169):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL PRIMERO')
         return 404
@@ -103,9 +101,9 @@ def riesgo_SEGUNDO(omnibus, momento):
             return Globales.RIESGO
         if (omnibus > -0.530380796 and omnibus <= -0.20261015):
             return Globales.BAJO
-        if (omnibus > -0.20261015 and omnibus <= 0.044400053):
+        if (omnibus > -0.20261015 and omnibus <= 0.109327853):
             return Globales.NORMAL
-        if (omnibus > 0.044400053):
+        if (omnibus > 0.109327853):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
@@ -113,11 +111,11 @@ def riesgo_SEGUNDO(omnibus, momento):
     if momento == Globales.MEDIO:
         if (omnibus <= -0.426241615):
             return Globales.RIESGO
-        if (omnibus > -0.426241615 and omnibus <= -0.268322849):
+        if (omnibus > -0.426241615 and omnibus <= -0.233464275):
             return Globales.BAJO
-        if (omnibus > -0.268322849 and omnibus <= 0.052567295):
+        if (omnibus > -0.233464275 and omnibus <= 0.177905594):
             return Globales.NORMAL
-        if (omnibus > 0.052567295):
+        if (omnibus > 0.177905594):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
@@ -125,11 +123,11 @@ def riesgo_SEGUNDO(omnibus, momento):
     if momento == Globales.FIN:
         if (omnibus <= -0.448651826):
             return Globales.RIESGO
-        if (omnibus > -0.448651826 and omnibus <= -0.120972641):
+        if (omnibus > -0.448651826 and omnibus <= -0.0688233010):
             return Globales.BAJO
-        if (omnibus > -0.120972641 and omnibus <= 0.140222415):
+        if (omnibus > -0.0688233010 and omnibus <= 0.2359250521):
             return Globales.NORMAL
-        if (omnibus > 0.140222415):
+        if (omnibus > 0.2359250521):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
@@ -143,9 +141,9 @@ def riesgo_TERCERO(omnibus, momento):
             return Globales.RIESGO
         if (omnibus > -0.651197192 and omnibus <= -0.302387656):
             return Globales.BAJO
-        if (omnibus > -0.302387656 and omnibus <= -0.032568676):
+        if (omnibus > -0.302387656 and omnibus <= 0.056823835):
             return Globales.NORMAL
-        if (omnibus > -0.032568676):
+        if (omnibus > 0.056823835):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
@@ -155,9 +153,9 @@ def riesgo_TERCERO(omnibus, momento):
             return Globales.RIESGO
         if (omnibus > -0.794233912 and omnibus <= -0.264449392):
             return Globales.BAJO
-        if (omnibus > -0.264449392 and omnibus <= -0.061627301):
+        if (omnibus > -0.264449392 and omnibus <= 0.041385219):
             return Globales.NORMAL
-        if (omnibus > -0.061627301):
+        if (omnibus > 0.041385219):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
@@ -167,9 +165,9 @@ def riesgo_TERCERO(omnibus, momento):
             return Globales.RIESGO
         if (omnibus > -0.682065767 and omnibus <= -0.243051133):
             return Globales.BAJO
-        if (omnibus > -0.243051133 and omnibus <= 0.005023444):
+        if (omnibus > -0.243051133 and omnibus <= 0.115224294):
             return Globales.NORMAL
-        if (omnibus > 0.005023444):
+        if (omnibus > 0.115224294):
             return Globales.OPTIMO
         print('>>> Error calculando RIESGO IPAL SEGUNDO')
         return 404
