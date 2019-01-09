5a2a326e40d5bc9977c85f1d1e4d575143903f0b
isaacmarco <isaacmarco@gmail.com>
2019-01-09 23:39:26 +0000

    T

--- rti/settings.py
+++ rti/settings.py
@@ -53,7 +53,7 @@ SECRET_KEY = find_or_create_secret_key()
 #	SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # direccion base
 PRODUCCION = True
